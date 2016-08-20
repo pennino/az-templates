1c43eddccb5499e1550814f99d3e5d57b87af71c
Ettore Carrara <etcarrar@microsoft.com>
2016-08-20 09:19:33 +0200

    Multi Region PaaS Sample

---
+++ Multi Site PaaS with TM/MultiSite-PaaS-Deploy.ps1
@@ -0,0 +1,39 @@
+﻿#region Login
+$cred = Get-Credential
+Login-AzureRmAccount -Credential $cred
+#endregion
+
+#region Create Resource Group
+$rg = "lab-paas-rg"
+$location = "westeurope"
+
+try {
+    Get-AzureRmResourceGroup -name $rg -Location $location -ea stop
+}
+catch {
+    write-host -ForegroundColor Yellow ("Creating Resource Group '{0}'" -f $rg)
+    $rg = New-AzureRmResourceGroup -name $rg -Location $location
+}
+#endregion
+
+
+#region deploy resources
+
+If ($psISE){
+    $root = split-path $psISE.CurrentFile.FullPath
+}
+
+$templateFilePath = join-path $root "MultiSite-PaaS.json"
+
+try {
+    Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName `
+    -TemplateFile $templateFilePath -verbose -ea Stop
+
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName `
+     -TemplateFile $templateFilePath -verbose   
+}
+catch {
+    write-host -ForegroundColor red "Check Deployment Status"
+}
+
+#endregion
