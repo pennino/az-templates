1c00e139956b93edb6b9dc1368a2b3ef0d63027a
Ettore Carrara <etcarrar@microsoft.com>
2016-08-25 10:52:33 +0200

    VM with Secret in Vault

---
+++ VM with VaultKeys/VM-VaultKeys-Deploy.ps1
@@ -0,0 +1,41 @@
+﻿#region Login
+$cred = Get-Credential
+Login-AzureRmAccount -Credential $cred
+#endregion
+
+#region Create Resource Group
+$rg = "test-keyvault-rg"
+$location = "westeurope"
+
+try {
+    Get-AzureRmResourceGroup -name $rg -Location $location -ea stop
+}
+catch {
+    write-host -ForegroundColor Yellow ("Creating Resource Group '{0}'" -f $rg)
+    $rg = New-AzureRmResourceGroup -name $rg -Location $location
+}
+#endregion
+
+
+#region deploy resources
+
+If ($psISE){
+    $root = split-path $psISE.CurrentFile.FullPath
+}
+
+$templateFilePath = join-path $root "VM-VaultKeys-Template.json"
+$parametersFilePath = join-path $root "VM-VaultKeys-Template-Params.json"
+
+try {
+    Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName `
+    -TemplateFile $templateFilePath -TemplateParameterFile $parametersFilePath -verbose -ea Stop
+
+    New-AzureRmResourceGroupDeployment -ResourceGroupName $rg.ResourceGroupName  -TemplateFile $templateFilePath `
+    -TemplateParameterFile $parametersFilePath -verbose   
+}
+catch {
+    write-host -ForegroundColor red "Check Deployment Status"
+}
+
+#endregion
+
